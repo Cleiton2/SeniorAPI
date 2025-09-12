829ebb2baed10a5e26481d08790692de2ba7ef31
Joao Vitor Rezende <virror26@gmail.com>
2025-09-12 12:07:09 -0300

    Commit 5 - injeta dependencia no servico de pessoa, para utilizar nos testes e atender principios da programação

---
+++ SeniorAPI/Service/Interfaces/IPessoaService.cs
@@ -0,0 +1,15 @@
+﻿using SeniorAPI.Enumerador;
+using SeniorAPI.Model;
+
+namespace SeniorAPI.Service.Interfaces
+{
+    public interface IPessoaService
+    {
+        PessoaModel Adicionar(PessoaModel pessoaModel);
+        List<PessoaModel> ObterPessoas();
+        PessoaModel? ObterPessoaPorCodigo(int codigo);
+        List<PessoaModel> ObterPessoasPorUF(UF UF);
+        void DeletarPessoa(int codigo);
+        PessoaModel EditarPessoa(int codigo, PessoaModel pessoaModel);
+    }
+}
