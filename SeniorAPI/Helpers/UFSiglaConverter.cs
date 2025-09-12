42c1bbfae33e00d6d9fec89b4a24597d6a169897
Joao Vitor Rezende <virror26@gmail.com>
2025-09-12 12:06:25 -0300

    Commit 4 - adiciona helper de conversão de sigla para uf

---
+++ SeniorAPI/Helpers/UFSiglaConverter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SeniorAPI.Enumerador;
+
+namespace SeniorAPI.Helpers
+{
+    public class UFSiglaConverter : StringEnumConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+            {
+                string? ufString = reader?.Value?.ToString();
+                if (Enum.TryParse(ufString, true, out UF ufEnum))
+                {
+                    return ufEnum;
+                }
+            }
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is UF uf)
+            {
+                writer.WriteValue(uf.ToString());
+            }
+            else
+            {
+                base.WriteJson(writer, value, serializer);
+            }
+        }
+    }
+}
